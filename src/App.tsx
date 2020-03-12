e738d1975178e6263e9a04c030d9d0dcb79f1de8
DenimCity <altidorj1@gmail.com>
2020-03-11 22:55:30 -0400

    setting up Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,72 @@
-import React, { useState, Fragment } from 'react';
-import logo from './logo.svg';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
 
+import { Store } from './Store'
+import { IEpisode, IAction } from './interface';
 
-interface ITodo {
-  text: string
-  completed: boolean
-}
-type FormElement = React.FormEvent<HTMLFormElement>
+
+
 export default function App(): JSX.Element {
-  const [value, setValue] = useState<string>('')
-  const [todos, setTodos] = useState<ITodo[]>([])
+  const { state, dispatch } = React.useContext(Store)
+  console.log("state, dispatch", state.episodes)
 
+  useEffect(() => {
+    state.episodes.length === 0 && fetchData()
+  }, [])
 
+  const fetchData = async () => {
+    const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+    const data = await fetch(URL)
+    const dataJson = await data.json()
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJson._embedded.episodes
 
-
-  const handleSubmit = (e: FormElement): void => {
-    e.preventDefault()
-    addToDo(value)
-    setValue('')
+    })
   }
 
-  const addToDo = (text: string): void => {
-    const newTodos: ITodo[] = [...todos, { text, completed: false }]
-    setTodos(newTodos)
-  }
-
-  const completeTodo = (index: number): void => {
-    const newTodo: ITodo[] = [...todos]
-    newTodo[index].completed = !newTodo[index].completed
-    setTodos(newTodo)
-  }
-
-  const removeTodo = (index: number): void => {
-    const newTodos: ITodo[] = [...todos]
-    newTodos.splice(index, 1)
-    setTodos(newTodos)
+  const toggleFavAction = (episode: IEpisode): IAction => {
+    const episodeInFave = state.favorites.includes(episode)
+    let dispatchObj = {
+      "type": 'ADD_FAV',
+      "payload": episode
+    }
+    if (episodeInFave) {
+      const favoritesWithoutEpisode = state.favorites.filter((fav: IEpisode) => fav.id !== episode.id)
+      dispatchObj = {
+        "type": 'REMOVE_FAV',
+        "payload": favoritesWithoutEpisode
+      }
+    }
+    return dispatch(dispatchObj)
   }
   return (
     <Fragment >
-      <h1>Todo List</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={(e) => setValue(e.target.value)} required />
-        <button type="submit">Add Todo</button>
-      </form>
-      <section>
-        {todos.map((todo: ITodo, index: number) => (
-          <Fragment key={index}>
-            <div style={{ textDecoration: todo.completed ? 'line-through' : '' }} key={index}>{todo.text}</div>
-            <button type="button" onClick={() => completeTodo(index)}>{todo.completed ? 'Incomplete' : 'Complete'}</button>
-            <button type="button" onClick={() => removeTodo(index)}>x</button>
-          </Fragment>
-        )
-        )}
+      <section style={{ display: 'flex', justifyContent: 'space-between', margin: '0 5vw', borderBottom: 'double #f65a5f' }}>
+        <h1>Rick And Morty</h1>
+        <p>Pick Youre Favorite episode</p>
+      </section>
+      <section style={{ display: 'flex', flexWrap: "wrap" }}>
+        {state.episodes.map((episode: IEpisode) => {
+          return (
+            <section style={{ margin: '5px 24px' }} key={episode.id}>
+              <img src={episode.image.medium} alt={`Rick and Mort ${episode.name}`} />
+              <div>{episode.name}</div>
+              <section>
+                Season: {episode.season} Number: {episode.number}
+              </section>
+              <button type="button" onClick={() => toggleFavAction(episode)}>
+                {state.favorites.find((fav: IEpisode) => fav.id === episode.id) ? 'UnFav' : 'FAV'}
+
+              </button>
+            </section>
+          )
+        })}
       </section>
     </Fragment>
   );
 }
+
 
 
