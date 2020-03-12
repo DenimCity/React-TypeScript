28693463af40f86d351069013e7824c59efbeafe
DenimCity <altidorj1@gmail.com>
2020-03-11 23:25:15 -0400

    using react lazy and reat suspense

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import './App.css';
 import { Store } from './Store'
 import { IEpisode, IAction } from './interface';
 
-
+const EpisodeList = React.lazy<any>(() => import('./components/Episodes'))
 
 export default function App(): JSX.Element {
   const { state, dispatch } = React.useContext(Store)
   console.log("state, dispatch", state.episodes)
+
 
   useEffect(() => {
     state.episodes.length === 0 && fetchData()
@@ -41,29 +42,30 @@ export default function App(): JSX.Element {
     }
     return dispatch(dispatchObj)
   }
+
+  const props = {
+    episodes: state.episodes,
+    toggleFavAction,
+    favorites: state.favorites
+  }
   return (
     <Fragment >
-      <section style={{ display: 'flex', justifyContent: 'space-between', margin: '0 5vw', borderBottom: 'double #f65a5f' }}>
-        <h1>Rick And Morty</h1>
-        <p>Pick Youre Favorite episode</p>
-      </section>
-      <section style={{ display: 'flex', flexWrap: "wrap" }}>
-        {state.episodes.map((episode: IEpisode) => {
-          return (
-            <section style={{ margin: '5px 24px' }} key={episode.id}>
-              <img src={episode.image.medium} alt={`Rick and Mort ${episode.name}`} />
-              <div>{episode.name}</div>
-              <section>
-                Season: {episode.season} Number: {episode.number}
-              </section>
-              <button type="button" onClick={() => toggleFavAction(episode)}>
-                {state.favorites.find((fav: IEpisode) => fav.id === episode.id) ? 'UnFav' : 'FAV'}
+      <header style={{ display: 'flex', justifyContent: 'space-between', margin: '0 5vw', borderBottom: 'double #f65a5f', alignItems: 'center' }}>
 
-              </button>
-            </section>
-          )
-        })}
-      </section>
+        <div>
+          <h1>Rick And Morty</h1>
+          <p>Pick Youre Favorite episode</p>
+        </div>
+
+        <div>
+          Favorite(s): {state.favorites.length}
+        </div>
+      </header>
+      <React.Suspense fallback={'Loading'}>
+        <section style={{ display: 'flex', flexWrap: "wrap" }}>
+          <EpisodeList {...props} />
+        </section>
+      </React.Suspense>
     </Fragment>
   );
 }
