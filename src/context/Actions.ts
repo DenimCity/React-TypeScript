7eb532d3a2f9ce99b9a5de7d4f730d078e8e23e6
DenimCity <altidorj1@gmail.com>
2020-03-12 08:49:03 -0400

    used  avtion variable to eliminate grammar errors

--- src/context/Actions.ts
+++ src/context/Actions.ts
@@ -1,27 +1,36 @@
 import { IState, IEpisode, IAction } from '../interface';
+import { FETCH_DATA, ADD_FAV, REMOVE_FAV } from './actionTypes'
 
 export const fetchData = async (dispatch: any) => {
-    const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
-    const data = await fetch(URL)
-    const dataJson = await data.json()
-    return dispatch({
-        type: 'FETCH_DATA',
-        payload: dataJson._embedded.episodes
+    try {
+        const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+        const data = await fetch(URL)
+        const dataJson = await data.json()
 
-    })
+        return dispatch({
+            type: FETCH_DATA,
+            payload: dataJson._embedded.episodes
+        })
+
+    } catch (error) {
+        /**
+         * Create An Error State
+         * If Error Dispatch *something*
+         */
+    }
 }
 
 export const toggleFavAction = (state: IState, dispatch: any, episode: IEpisode | any): IAction => {
     const episodeInFave = state.favorites.includes(episode)
     let dispatchObj = {
-        "type": 'ADD_FAV',
-        "payload": episode
+        type: ADD_FAV,
+        payload: episode
     }
     if (episodeInFave) {
         const favoritesWithoutEpisode = state.favorites.filter((fav: IEpisode) => fav.id !== episode.id)
         dispatchObj = {
-            "type": 'REMOVE_FAV',
-            "payload": favoritesWithoutEpisode
+            type: REMOVE_FAV,
+            payload: favoritesWithoutEpisode
         }
     }
     return dispatch(dispatchObj)
--- src/context/Store.tsx
+++ src/context/Store.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IState, IAction } from '../interface'
+import { FETCH_DATA, ADD_FAV, REMOVE_FAV } from './actionTypes'
 
 const INITIAL_STATE: IState = {
     episodes: [],
@@ -8,11 +9,11 @@ const INITIAL_STATE: IState = {
 
 const reducer = (state: IState, action: IAction) => {
     switch (action.type) {
-        case 'FETCH_DATA':
+        case FETCH_DATA:
             return { ...state, episodes: action.payload }
-        case 'ADD_FAV':
+        case ADD_FAV:
             return { ...state, favorites: [...state.favorites, action.payload] }
-        case 'REMOVE_FAV':
+        case REMOVE_FAV:
             return { ...state, favorites: action.payload }
         default:
             return state
